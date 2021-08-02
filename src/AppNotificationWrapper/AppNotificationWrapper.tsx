7e418944a3372a0e3417b79c0c54d43fe0a70d80
Minnia <jasminestockholm@gmail.com>
2021-08-02 14:31:31 +0200

    change onPress in AppNotificationWrapper, remove onPress in AppNotificationUI

--- src/AppNotificationWrapper/AppNotificationWrapper.tsx
+++ src/AppNotificationWrapper/AppNotificationWrapper.tsx
@@ -1,6 +1,11 @@
 import { useAnimatedValue, useLayout } from "@redmindab/react-hooks";
 import React, { Fragment, FunctionComponent, useRef, useState } from "react";
-import { Animated, Dimensions, PanResponder } from "react-native";
+import {
+  Animated,
+  Dimensions,
+  PanResponder,
+  TouchableOpacity,
+} from "react-native";
 import { View } from "styled-native-kit";
 import {
   AppNotificationComponentProps,
@@ -18,18 +23,29 @@ type WrapperProps = {
   reversed?: boolean;
 };
 
+type TouchableOpacityProps = {
+  activeOpacity: number;
+  onPress: () => void;
+};
+
 export const AppNotificationWrapper: FunctionComponent<Props> = ({
   animateOut,
   animated = true,
   panEnabled = true,
   alignBottom,
-  children,
   onPress,
+  children,
   animationWrappers = { in: undefined, out: undefined },
 }) => {
   const [hide, setHide] = useState(false);
   const [layout, bindLayout] = useLayout();
   const translateX = useAnimatedValue(0);
+
+  const touchableProps: TouchableOpacityProps = {
+    activeOpacity: 1,
+    onPress: () => null,
+  };
+
   const pan = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponderCapture: () => true,
@@ -38,8 +54,9 @@ export const AppNotificationWrapper: FunctionComponent<Props> = ({
       },
       onPanResponderRelease: (e, { vx, dx }) => {
         const hasTapped = vx + dx === 0;
-        if (onPress && hasTapped) {
-          onPress();
+        if (onPress && !panEnabled && hasTapped) {
+          touchableProps.onPress = onPress();
+          touchableProps.activeOpacity = 0.7;
         }
         const screenWidth = Dimensions.get("window").width;
         if (Math.abs(vx) >= 0.5 || Math.abs(dx) >= 0.5 * screenWidth) {
@@ -91,7 +108,9 @@ export const AppNotificationWrapper: FunctionComponent<Props> = ({
   return (
     <AnimationWrapper {...wrapperProps}>
       <PanWrapper {...panWrapperProps}>
-        <View {...bindLayout}>{children}</View>
+        <View {...bindLayout}>
+          <TouchableOpacity {...touchableProps}>{children}</TouchableOpacity>
+        </View>
       </PanWrapper>
     </AnimationWrapper>
   );
