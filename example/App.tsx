2ffad6f3f0abeb288436bdbb1e2dc37258e37454
Sascha Ringström <sascharingstrom@gmail.com>
2024-04-04 22:02:00 +0200

    Updated example project

--- example/App.tsx
+++ example/App.tsx
@@ -1,54 +1,57 @@
-import React, {useRef} from 'react';
-import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import { Alert, SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native'
 
-import AppNotification from './react-native-app-notification/src';
-import {NotificationOptions} from './react-native-app-notification/src/types';
+import { useRef } from 'react'
+import AppNotification from './react-native-app-notification/src'
+import { NotificationOptions } from './react-native-app-notification/src/types'
 
 const notifications: NotificationOptions[] = [
   {
     title: 'Your title',
-    message: 'Your message',
+    message: 'Press me',
+    onPress: () => Alert.alert('Pressed'),
+  },
+  {
+    title: 'Unswipeable',
+    message: 'Press me',
+    panEnabled: false,
+    onPress: () => Alert.alert('Pressed'),
   },
   {
     title: 'Saved changes',
     message: 'Your profile has been updated',
-    right: <Text style={{color: 'blue'}}>Revert</Text>,
+    right: <Text style={{ color: 'blue' }}>Revert</Text>,
   },
   {
     imageUrl: 'https://picsum.photos/80/80',
     title: 'Message from John Doe',
     message: 'Hello, how are you?',
   },
-];
+]
 
-function App(): JSX.Element {
-  const indexRef = useRef(0);
+export default function App() {
+  const indexRef = useRef(0)
 
   const onPress = () => {
-    if (indexRef.current === notifications.length) indexRef.current = 0;
-    const options = notifications[indexRef.current++];
+    if (indexRef.current === notifications.length) indexRef.current = 0
+    const options = notifications[indexRef.current++]
 
-    AppNotification.show({duration: 1000 * 3, ...options});
-  };
+    AppNotification.show({ duration: 1000 * 3, ...options })
+  }
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
-      <View style={{flex: 1, justifyContent: 'flex-end'}}>
-        <TouchableOpacity
-          onPress={onPress}
-          activeOpacity={0.8}
-          style={{
-            backgroundColor: 'black',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flex: 1,
-          }}
-        >
-          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20}}>
-            Press Anywhere
-          </Text>
-        </TouchableOpacity>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={onPress}
+        activeOpacity={0.8}
+        style={{
+          backgroundColor: 'black',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
+        }}
+      >
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>Press Anywhere</Text>
+      </TouchableOpacity>
       <AppNotification
       // Add custom animations!
       // animationWrappers={{
@@ -57,7 +60,14 @@ function App(): JSX.Element {
       // }}
       />
     </SafeAreaView>
-  );
+  )
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
